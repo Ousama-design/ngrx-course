2f8148674d40902c016f668c535d782ab5cb3ec9
ousama-fd <ousama.fakhreddine@nextcarehealth.com>
2023-04-18 10:33:47 +0300

    refactoring store state

--- src/app/counter/counter-output/counter-output.component.ts
+++ src/app/counter/counter-output/counter-output.component.ts
@@ -1,20 +1,33 @@
-import { Component,OnInit } from '@angular/core';
+import { Component,OnInit,OnDestroy } from '@angular/core';
 import {Store} from "@ngrx/store";
+import { counterState } from '../state/counter.state';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-counter-output',
   templateUrl: './counter-output.component.html',
   styleUrls: ['./counter-output.component.css']
 })
-export class CounterOutputComponent implements OnInit{
+export class CounterOutputComponent implements OnInit,OnDestroy{
   
   counter:number=0;
-  
-  constructor(private store:Store<{counter:{counter:number}}>){
+  counterSubscription!:Subscription;
+  counter$?:Observable<{counter:number}>
+  constructor(private store:Store<{counter:counterState}>){
   }  
 
   ngOnInit(){
-		this.store.select('counter').subscribe((data)=>{
+		this.counterSubscription=this.store
+    .select('counter')
+    .subscribe((data)=>{
 	this.counter=data.counter;})
+
+  this.counter$=this.store.select('counter');
 	}
+
+  ngOnDestroy(): void {
+    if(this.counterSubscription){
+      this.counterSubscription.unsubscribe();
+    }
+  }
 }
--- src/app/counter/state/counter.state.ts
+++ src/app/counter/state/counter.state.ts
@@ -1,3 +1,6 @@
+export interface counterState{
+    counter:0
+}
 export const initialState={
     counter:0
 }
